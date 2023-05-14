6b7894f0b8fdd3bdd73ce77907f83b3589f23ce1
Jinwoo Kim <jinwookim@kw.ac.kr>
2023-05-14 11:44:10 +0900

    update bash script

--- build_image.sh
+++ build_image.sh
@@ -1,1 +1,7 @@
-docker build -t cs155-proj2-image .
+#!/bin/bash
+
+if [[ "$OSTYPE" == "msys" ]]; then
+    winpty docker build -t cs155-proj2-image .
+else
+    docker build -t cs155-proj2-image .
+fi
--- start_server.sh
+++ start_server.sh
@@ -1,1 +1,7 @@
-docker run -it --rm -p 3000:3000 --mount type=bind,source="$(pwd)"/code/router.js,target=/home/cs155/proj2/router.js --mount type=bind,source="$(pwd)"/code/views,target=/home/cs155/proj2/views --name bitbar-container cs155-proj2-image
+#!/bin/bash
+
+if [[ "$OSTYPE" == "msys" ]]; then
+    docker run -it --rm -p 3000:3000 --mount type=bind,source="/$(pwd)"/code/router.js,target=/home/cs155/proj2/router.js --mount type=bind,source="/$(pwd)"/code/views,target=/home/cs155/proj2/views --name bitbar-container cs155-proj2-image
+else
+    docker run -it --rm -p 3000:3000 --mount type=bind,source="$(pwd)"/code/router.js,target=/home/cs155/proj2/router.js --mount type=bind,source="$(pwd)"/code/views,target=/home/cs155/proj2/views --name bitbar-container cs155-proj2-image
+fi
